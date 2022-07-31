b00b58b5e7929f6e04fff20c78e32e03d9b1cf83
Peter <p33tcz@gmail.com>
2022-07-31 02:04:47 +0200

    🦁 init

---
+++ assets/js/ftr_buttons.js
@@ -0,0 +1,22 @@
+// 2022 copyright - P33t.net
+const proc = document.getElementById('proc');
+const kontakt = document.getElementById('kontakt');
+
+proc.addEventListener('click', function() {
+    swal("Proč tento web?", `
+    Tento web vznikl jako "památník" starého lsrp.cz serveru. Bohužel doména expirovala a místo toho aby to koupil další kid server, jsem se rozhodl doménu zakoupit já.
+    `);
+    
+}
+);
+
+kontakt.addEventListener('click', function() {
+    swal("Kontakt", `
+    Discord: P33t#9999
+    Email: p33tcz@gmail.com nebo p33t@p33t.net
+    Web: https://p33t.net
+    Můj roleplay projekt: https://zenrp.cz
+    `);
+   return; 
+}
+);
